e120d045a8878c51b488b864dbbd08f82984bae8
Harsh Gupta <harsh.iit999@gmail.com>
2019-11-16 06:25:24 +0530

    updated

--- parse.py
+++ parse.py
@@ -5,4 +5,5 @@ from pandas.io.json import json_normalize
 with open('imdb.json') as imdb_json:
     json_data=json.load(imdb_json)
 json_file = pd.DataFrame.from_dict(json_normalize(json_data))
+json_file.sort_values("rating",ascending=False,inplace=True)
 print(json_file)
