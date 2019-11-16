b37188c7f4b2e976ea24c3b8c40953128d5ebe6e
Harsh Gupta <harsh.iit999@gmail.com>
2019-11-16 22:31:29 +0530

    ouput  to excel file

--- parse.py
+++ parse.py
@@ -1,9 +1,11 @@
 import json
 import pandas as pd
+import openpyxl
 from pandas.io.json import json_normalize
 
 with open('imdb.json') as imdb_json:
     json_data=json.load(imdb_json)
 json_file = pd.DataFrame.from_dict(json_normalize(json_data))
 json_file.sort_values("rating",ascending=False,inplace=True)
-print(json_file)
+json_file.to_excel('final_output.xlsx')
+
