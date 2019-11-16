dde7779d9ab5787a10945730b15729f2ab19ea28
Harsh Gupta <harsh.iit999@gmail.com>
2019-11-16 06:24:54 +0530

    updated

--- scrapper.py
+++ scrapper.py
@@ -6,14 +6,13 @@ import json
 def preprocess (url):
     response = requests.get(url, timeout=10)
     content = BeautifulSoup(response.content, 'html.parser')
-    for movie in content.find_all('td', attrs={'class': 'titleColumn'}):
+    for movie in content.find_all('div', attrs={'class': 'lister-item-content'}):
         name = movie.find('a').get_text()
-        date = movie.find('span', attrs={'class': 'secondaryInfo'}).get_text()
+        rating = movie.find('strong').get_text()
+        date = movie.find('span', attrs={'class': 'lister-item-year text-muted unbold'}).get_text()
         movie_names.append(name)
         movie_year.append(date)
-    for movie in content.find_all('td', attrs={'class': 'ratingColumn imdbRating'}):
-        name = movie.find('strong').get_text()
-        movie_imdbrating.append(name)
+        movie_imdbrating.append(rating)
     sz = len(movie_names)
     for i in range(0, sz):
         data = {
@@ -23,15 +22,37 @@ def preprocess (url):
         }
         movies.append(data)
 
-
-
 movie_names = []
 movie_year = []
 movie_imdbrating = []
 movies = []
 
-
-preprocess ('https://www.imdb.com/chart/top')
+genre_to_url = {
+    'action': 'https://www.imdb.com/search/title/?genres=action&sort=num_votes,desc&explore=title_type,genres',
+    'adventure': 'https://www.imdb.com/search/title/?genres=adventure&sort=num_votes,desc&explore=title_type,genres',
+    'animation': 'https://www.imdb.com/search/title/?genres=animation&sort=num_votes,desc&explore=title_type,genres',
+    'biography': 'https://www.imdb.com/search/title/?genres=biography&sort=num_votes,desc&explore=title_type,genres',
+    'comedy': 'https://www.imdb.com/search/title/?genres=comedy&sort=num_votes,desc&explore=title_type,genres',
+    'crime': 'https://www.imdb.com/search/title/?genres=crime&sort=num_votes,desc&explore=title_type,genres',
+    'documentary': 'https://www.imdb.com/search/title/?genres=documentary&sort=num_votes,desc&explore=title_type,genres',
+    'drama': 'https://www.imdb.com/search/title/?genres=drama&sort=num_votes,desc&explore=title_type,genres',
+    'family': 'https://www.imdb.com/search/title/?genres=family&sort=num_votes,desc&explore=title_type,genres',
+    'fantasy': 'https://www.imdb.com/search/title/?genres=fantasy&sort=num_votes,desc&explore=title_type,genres',
+    'film-noir': 'https://www.imdb.com/search/title/?genres=film-noir&sort=num_votes,desc&explore=title_type,genres',
+    'history': 'https://www.imdb.com/search/title/?genres=history&sort=num_votes,desc&explore=title_type,genres',
+    'horror': 'https://www.imdb.com/search/title/?genres=horror&sort=num_votes,desc&explore=title_type,genres',
+    'music': 'https://www.imdb.com/search/title/?genres=music&sort=num_votes,desc&explore=title_type,genres',
+    'musical': 'https://www.imdb.com/search/title/?genres=musical&sort=num_votes,desc&explore=title_type,genres',
+    'mystery': 'https://www.imdb.com/search/title/?genres=mystery&sort=num_votes,desc&explore=title_type,genres',
+    'romance': 'https://www.imdb.com/search/title/?genres=romance&sort=num_votes,desc&explore=title_type,genres',
+    'sci-fi': 'https://www.imdb.com/search/title/?genres=sci-fi&sort=num_votes,desc&explore=title_type,genres',
+    'sport': 'https://www.imdb.com/search/title/?genres=sport&sort=num_votes,desc&explore=title_type,genres',
+    'thriller': 'https://www.imdb.com/search/title/?genres=thriller&sort=num_votes,desc&explore=title_type,genres',
+    'war': 'https://www.imdb.com/search/title/?genres=war&sort=num_votes,desc&explore=title_type,genres',
+    'western': 'https://www.imdb.com/search/title/?genres=western&sort=num_votes,desc&explore=title_type,genres',
+}
+genre = input('Enter your genre ').lower()
+preprocess(genre_to_url[genre])
 
 
 with open('imdb.json', 'w') as output:
